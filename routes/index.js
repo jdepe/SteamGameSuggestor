2e9f89ee63cd01721997ec5919e9d537ba398f8e
jdepe <jaden.dep@gmail.com>
2023-09-02 17:27:23 +1000

    list of games that friends play and i dont own

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,60 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Docker Mashup' });
 });
 
+// Steam
+router.get("/suggested-games", async function (req, res) {
+  const STEAM_KEY = process.env.STEAM_API_KEY;
+  const steam_id = "76561198032549999";
+  const steam_url = "http://api.steampowered.com/"
+
+  // FIRST CALL TO GET OWNED GAMES LIST
+  const firstRes = await fetch(`${steam_url}IPlayerService/GetOwnedGames/v0001/?key=${STEAM_KEY}&steamid=${steam_id}&include_appinfo=true`)
+  const ownedGameData = await firstRes.json();
+  let gamesList = ownedGameData.response.games;
+  gamesList = gamesList.map(game => ({
+    name: game.name,
+    appid: game.appid,
+  }))
+
+  // // SECOND CALL TO GET FRIENDS LIST
+  const secondRes = await fetch(`${steam_url}ISteamUser/GetFriendList/v0001/?key=${STEAM_KEY}&steamid=${steam_id}&relationship=friend`);
+  const friendData = await secondRes.json();
+  const firstFiveFriends = friendData.friendslist.friends.slice(0,5);
+  
+  // THIRD CALL TO GET EACH FRIENDS RECENTLY PLAYED GAMES
+  async function fetchRecentlyPlayedGames(steamid) {
+    const response = await fetch(`${steam_url}IPlayerService/GetRecentlyPlayedGames/v0001/?key=${STEAM_KEY}&steamid=${steamid}&format=json`);
+    const data = await response.json();
+    return data;
+  }
+
+  let recentGames = [];
+
+  for (const friend of firstFiveFriends) {
+    const result = await fetchRecentlyPlayedGames(friend.steamid);
+    recentGames.push(result);
+  }
+
+  const flattenedGames = recentGames.map(item => item.response.games || []).flat();
+
+  const uniqueGamesMap = {};
+  const uniqueGames = [];
+
+  flattenedGames.forEach(game => {
+    if (!uniqueGamesMap[game.appid]) {
+      uniqueGamesMap[game.appid] = true;
+      uniqueGames.push(game)
+    }    
+  });
+
+  // Compare friends recent games to your owned games
+  const gamesNotOwned = uniqueGames.filter(friendGame => 
+    !gamesList.some(myGame => myGame.appid === friendGame.appid));
+
+  console.log(gamesNotOwned);
+
+
+  res.render('steam-search', {query: 'me', results: gamesNotOwned});
+});
+
 module.exports = router;
